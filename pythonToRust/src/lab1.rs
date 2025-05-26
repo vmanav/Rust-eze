dedb39ab40e1118945ea9698af01b531af24befd
Manav Verma <zmanav.1999@gmail.com>
2025-05-26 21:17:07 +0530

    add 26/5

--- pythonToRust/src/lab1.rs
+++ pythonToRust/src/lab1.rs
@@ -19,8 +19,21 @@ pub fn q1() {
     println!("Sum2 = {}", number * (number + 1) / 2);
 }
 
-pub fn q1() {}
+pub fn q2() {
+    let mut input = String::new();
+    let PIE = 3.14;
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let number: f32 = input.trim().parse().expect("Failed to parse line");
+    println!("Number: {}", number);
+    let area = PIE * number * number;
+    println!("Area = {}", area);
+}
 
 pub fn run() {
-    q1();
+    // q1();
+    q2();
 }
