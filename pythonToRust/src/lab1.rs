30a319d3562d8ad40114cc21d60c80b0404165c5
Manav Verma <zmanav.1999@gmail.com>
2025-05-27 22:47:18 +0530

    add lab1

--- pythonToRust/src/lab1.rs
+++ pythonToRust/src/lab1.rs
@@ -1,3 +1,5 @@
+use std::cmp;
+use std::env;
 use std::io;
 
 pub fn q1() {
@@ -21,7 +23,7 @@ pub fn q1() {
 
 pub fn q2() {
     let mut input = String::new();
-    let PIE = 3.14;
+    let pie = 3.14;
 
     io::stdin()
         .read_line(&mut input)
@@ -29,11 +31,95 @@ pub fn q2() {
 
     let number: f32 = input.trim().parse().expect("Failed to parse line");
     println!("Number: {}", number);
-    let area = PIE * number * number;
+    let area = pie * number * number;
     println!("Area = {}", area);
+}
+
+pub fn q4() {
+    let args: Vec<String> = env::args().collect();
+    let sec = 60;
+
+    println!("Enter time in this format (DAYS) (HOURS) (MINS) (SECONDS)");
+    let days: i32 = args[1]
+        .clone()
+        .trim()
+        .parse()
+        .expect("Failed to parse line");
+    let hours: i32 = args[1]
+        .clone()
+        .trim()
+        .parse()
+        .expect("Failed to parse line");
+    let min: i32 = args[1]
+        .clone()
+        .trim()
+        .parse()
+        .expect("Failed to parse line");
+    let seconds: i32 = args[1]
+        .clone()
+        .trim()
+        .parse()
+        .expect("Failed to parse line");
+
+    let total_time = seconds + min * sec + min * sec * sec + hours * sec * sec * sec;
+    println!("Total time: {}", total_time);
+}
+
+pub fn q6() {
+    let args: Vec<String> = env::args().collect();
+
+    println!("Enter three sides of triangle (space seperated)");
+    let s1: f32 = args[1]
+        .clone()
+        .trim()
+        .parse()
+        .expect("Failed to parse line");
+    let s2: f32 = args[2]
+        .clone()
+        .trim()
+        .parse()
+        .expect("Failed to parse line");
+    let s3: f32 = args[3]
+        .clone()
+        .trim()
+        .parse()
+        .expect("Failed to parse line");
+
+    let s: f32 = (s1 + s2 + s3) / 2.0;
+    let area: f32 = ((s) * (s - s1) * (s - s2) * (s - s3)).sqrt();
+    println!("Total area: {}", area);
+}
+
+pub fn q7() {
+    let args: Vec<String> = env::args().collect();
+
+    println!("Enter three integers (space seperated)");
+    let a: i32 = args[1]
+        .clone()
+        .trim()
+        .parse()
+        .expect("Failed to parse line");
+    let b: i32 = args[2]
+        .clone()
+        .trim()
+        .parse()
+        .expect("Failed to parse line");
+    let c: i32 = args[3]
+        .clone()
+        .trim()
+        .parse()
+        .expect("Failed to parse line");
+
+    let min = cmp::min(a, cmp::min(b, c));
+    let max = cmp::max(a, cmp::max(b, c));
+    let middle = a + b + c - min - max;
+    println!("Min: {}, Max: {}, Middle: {}.", min, max, middle);
 }
 
 pub fn run() {
     // q1();
-    q2();
+    // q2();
+    // q4();
+    // q6();
+    q7();
 }
