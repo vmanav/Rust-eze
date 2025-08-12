4c995e57c055825a93dc9f41b024a08f594cd560
Manav Verma <zmanav.1999@gmail.com>
2025-08-12 11:32:33 +0100

    fix cors

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ app = FAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://thesis-fe-c06n.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
